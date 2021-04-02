c2cd6e59ebdd8aadd060610fc8c341786d0c46ec
nfach98 <nfach98@gmail.com>
2021-04-02 19:47:44 +0700

    register page

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,6 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
         'id_pegawai',
         'password',
     ];
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,98 @@
 @extends('layouts.app')
 
+@section('styles')
+<style type="text/css">
+    html, body {
+        height: 100%;
+        margin: 0;
+    }
+
+    #app{
+        height: 100%;
+        background-image: url({{ asset('images/background_login.jpg') }});
+        background-repeat:no-repeat;
+        background-size:cover;
+    }
+</style>
+@endsection
+
 @section('content')
-<div class="container">
+<div id="app">
+    <div class="container h-100">
+        <div class="row d-flex h-100">
+            <div class="col-md-10 my-auto mx-auto">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-5 my-auto">
+                                <h3 style="font-weight: 600;">Daftar untuk bergabung di {{config('app.name')}}</h3>
+                            </div>
+
+                            <div class="col-md-1">  
+                            </div>
+
+                            <div class="col-md-6">
+                                <form method="POST" action="{{ route('login') }}" style="text-align: center;">
+                                    @csrf
+
+                                    <div class="form-group row mb-3">
+                                        <div class="col-12">
+                                            <input id="id-pegawai" type="text" class="form-control @error('id_pegawai') is-invalid @enderror" name="id_pegawai" value="{{ old('id_pegawai') }}" required autocomplete="id_pegawai" placeholder="ID Pegawai">
+
+                                            @error('id_pegawai')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row mb-2">
+                                        <div class="col-12">
+                                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password"  placeholder="Password" data-toggle="password">
+
+                                            @error('password')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row mb-3">
+                                        <div class="col-12">
+                                            <div class="custom-control custom-checkbox mb-3">
+                                              <input type="checkbox" class="custom-control-input" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                              <label class="custom-control-label shadow-none" for="remember">
+                                                  {{ __('Remember Me') }}
+                                              </label>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row mb-5">
+                                        <div class="col-12">
+                                            <button type="submit" class="btn btn-primary shadow-none w-100">
+                                                {{ __('Login') }}
+                                            </button>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group mb-0">
+                                        <p class="mb-0">Ahmad Eka Fauzi © 2021</p>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -87,5 +178,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 @endsection
