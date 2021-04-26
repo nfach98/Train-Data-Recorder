689fff9fd1b9269f2bf140ae351f1e192062eecd
nfach98 <nfach98@gmail.com>
2021-04-26 20:17:35 +0700

    create monitoring route

---
+++ resources/views/monitoring.blade.php
@@ -0,0 +1,83 @@
+@extends('layouts.main')
+
+@section('page')
+  <div class="container-fluid" style="margin-top: 15px; margin-bottom: 15px;">
+      <div class="d-sm-flex align-items-center justify-content-between mb-4">
+          <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+          <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+      </div>
+
+      <div class="row">
+          <div class="col-xl-4 col-md-12 mb-4">
+              <div class="card shadow h-100 py-2">
+                  <div class="card-body">
+                      <div class="row no-gutters align-items-top">
+                          <div class="col mr-5">
+                              <div class="text-xs font-weight-bold text-uppercase mb-1">Listrik</div>
+                              <div class="row">
+                                  <div class="col text-xs">Arus</div>
+                                  <div class="col-4 text-right">2</div>
+                              </div>
+                              <div class="row">
+                                  <div class="col text-xs">Tegangan</div>
+                                  <div class="col-4 text-right">2</div>
+                              </div>
+                              <div class="row">
+                                  <div class="col text-xs">Frekuensi</div>
+                                  <div class="col-4 text-right">2</div>
+                              </div>
+                              <!-- <div class="h5 mb-0 font-weight-bold text-gray-800">27.5</div> -->
+                          </div>
+                          <div class="col-auto">
+                              <i class="fas fa-bolt fa-2x text-gray-300" style="color: var(--red);"></i>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+
+          <div class="col-xl-4 col-md-12 mb-4">
+              <div class="card shadow h-100 py-2">
+                  <div class="card-body">
+                      <div class="row no-gutters align-items-top">
+                          <div class="col mr-5">
+                              <div class="text-xs font-weight-bold text-uppercase mb-1">Lokasi</div>
+                              <div class="row">
+                                  <div class="col text-xs">Latitude</div>
+                                  <div class="col-4 text-right">2</div>
+                              </div>
+                              <div class="row">
+                                  <div class="col text-xs">Longitude</div>
+                                  <div class="col-4 text-right">2</div>
+                              </div>
+                              <div class="row">
+                                  <div class="col text-xs">Kecepatan</div>
+                                  <div class="col-4 text-right">2</div>
+                              </div>
+                          </div>
+                          <div class="col-auto">
+                              <i class="fas fa-map-marker-alt fa-2x text-gray-300" style="color: var(--red);"></i>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+
+          <div class="col-xl-4 col-md-12 mb-4">
+              <div class="card shadow h-100 py-2">
+                  <div class="card-body">
+                      <div class="row no-gutters align-items-top">
+                          <div class="col mr-2">
+                              <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Waktu Beroperasi</div>
+                              <div class="h5 mb-0 font-weight-bold text-gray-800">27.5</div>
+                          </div>
+                          <div class="col-auto">
+                              <i class="fas fa-stopwatch fa-2x text-gray-300" style="color: var(--red);"></i>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Events\FormSubmitted;
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/', [App\Http\Controllers\DashboardController::class, 'index']);
+
+Route::get('/monitoring', [App\Http\Controllers\MonitoringController::class, 'index']);
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
+Route::get('/home', [App\Http\Controllers\DashboardController::class, 'index'])->name('home');
 
 Route::get('/counter', function() {
 	return view('counter');
