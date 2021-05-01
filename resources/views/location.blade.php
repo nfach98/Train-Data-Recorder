9742b8020ee87d47217b01fb39643a1b7f17e2cb
nfach98 <nfach98@gmail.com>
2021-05-01 16:23:12 +0700

    location add user location & control

--- resources/views/location.blade.php
+++ resources/views/location.blade.php
@@ -6,14 +6,25 @@
         mapboxgl.accessToken = 'pk.eyJ1IjoibmZhY2g5OCIsImEiOiJja214aGkzd2Uwb3o1MnBtOWR0c3NyMGJvIn0.yIkMVzbHjJBHp7UHH4r4wQ';
         var map = new mapboxgl.Map({
           container: 'map',
-          style: 'mapbox://styles/mapbox/streets-v11'
+          style: 'mapbox://styles/mapbox/streets-v11',
+          center: [-96, 37.8],
+          zoom: 5
         });
+
+        map.addControl(new mapboxgl.GeolocateControl({
+            positionOptions: {
+            enableHighAccuracy: true
+            },
+            trackUserLocation: true
+          })
+        );
+        map.addControl(new mapboxgl.NavigationControl());
       }
     </script>
 @endsection
 
 @section('page')
-    <div class="container-fluid">
-        <div class="w-100" id="map" style='height: 300px;'></div>
+    <div class="container-fluid px-0 py-0">
+        <div class="w-100" id="map" style='height: calc(100vh - 80px);'></div>
     </div>
 @endsection
