05bd0f43bf46c5f824bf0fa8f618c81a9c9a2187
Nino Fachrurozy <nfach98@gmail.com>
2021-06-26 17:56:01 +0700

    minor

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-  <div class="d-flex" id="wrapper">
+  <!-- <div class="d-flex" id="wrapper">
       <div class="border-right" id="sidebar-wrapper">
           <a href="{{ url('/') }}" style="color: white;">
               <div class="d-flex flex-column justify-content-center">
@@ -81,9 +81,9 @@
                   <i class="fa fa-bars"></i>
               </button>
 
-              <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                   <span class="navbar-toggler-icon"></span>
-              </button> -->
+              </button>
 
               <ul class="navbar-nav ml-auto">
                   @guest
@@ -133,9 +133,104 @@
               </ul>
           </nav>
 
-          @yield('page')
       </div>
+  </div> -->
+
+  <div class="sidebar">
+      <ul class="sidebar-nav">
+         <!--  <a href="{{ url('/') }}" class="logo" style="color: white;">
+              <div class="d-flex flex-column justify-content-center">
+                  <img class="mx-auto my-1" src="{{ asset('images/logo_white.png') }}" alt="Logo TDR" style="height: 50%; width: 50%;">
+                  <h4 class="link-text" align="center">{{ config('app.name', 'Train Data Recorder') }}</h4>
+              </div>
+          </a> -->
+          <li class="logo">
+            <img src="{{ asset('images/logo_white.png') }}" alt="Logo TDR">
+          </li>
+
+          <li class="side-item">
+            <a href="{{ url('/') }}" class="side-link">
+              <i class="fas fa-tachometer-alt"></i>
+              <span class="link-text">Dashboard</span>
+            </a>
+          </li>
+
+          <li class="side-item">
+            <a href="{{ url('/monitoring') }}" class="side-link">
+              <i class="fas fa-desktop"></i>
+              <span class="link-text">Monitoring</span>
+            </a>
+          </li>
+
+          <li class="side-item">
+              <a href="{{ url('/location') }}" class="side-link">
+                <i class="fas fa-map-marker-alt"></i>
+                <span class="link-text">Location</span>
+              </a>
+          </li>
+      </ul>
   </div>
+
+  <main>
+      <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+          <!-- <button class="btn btn-transparent" id="menu-toggle">
+              <i class="fa fa-bars"></i>
+          </button>
+
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+              <span class="navbar-toggler-icon"></span>
+          </button> -->
+
+          <ul class="navbar-nav ml-auto">
+              @guest
+                  @if (Route::has('login'))
+                      <li class="nav-item">
+                          <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                      </li>
+                  @endif
+                  
+                  @if (Route::has('register'))
+                      <li class="nav-item">
+                          <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                      </li>
+                  @endif
+              @else
+                  <li class="nav-item dropdown">
+                      <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                          @if (is_null(Auth::user()->avatar))
+                              <img id="profile-avatar" class="img-profile rounded-circle mr-2" src="{{ asset('images/avatar_person.png') }}" style="background-color: {{ Auth::user()->color }};" 
+                              width="32" height="32">
+                          @else
+                              <img id="profile-avatar" class="img-profile rounded-circle mr-2" src="{{ Auth::user()->avatar }}"
+                              width="32" height="32">
+                          @endif
+                          {{ Auth::user()->name }}
+                      </a>
+
+                      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                          <a class="dropdown-item" href="#">
+                              <i class="fas fa-user"></i>
+                              Profile
+                          </a>
+
+                          <a class="dropdown-item" href="{{ route('logout') }}"
+                             onclick="event.preventDefault();
+                               document.getElementById('logout-form').submit();" style="color: var(--red);">
+                              <i class="fas fa-sign-out-alt"></i>
+                              {{ __('Logout') }}
+                          </a>
+
+                          <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                              @csrf
+                          </form>
+                      </div>
+                  </li>
+              @endguest
+          </ul>
+      </nav>
+
+      @yield('page')
+  </main>
 
   <script>
     $("#menu-toggle").click(function(e) {
