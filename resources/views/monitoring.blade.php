cda645346a9aa421dd9ef0f03d539f83ad4ae7bb
Nino Fachrurozy <nfach98@gmail.com>
2021-06-26 17:55:41 +0700

    add new graph monitoring

--- resources/views/monitoring.blade.php
+++ resources/views/monitoring.blade.php
@@ -3,6 +3,7 @@
 @section('script')
   <script type="text/javascript">
     window.onload = function () {
+      // Kemiringan
       var optionsSlope = {
         chart: {
           type: 'line',
@@ -18,10 +19,10 @@
           categories: [1991,1992,1993,1994,1995,1996,1997,1998,1999]
         }
       }
-
       var chartSlope = new ApexCharts(document.querySelector("#chart-slope"), optionsSlope);
       chartSlope.render();
 
+      // Suhu
       var optionsTemperature = {
         chart: {
           type: 'line',
@@ -36,9 +37,28 @@
           categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
         }
       }
-
       var chartTemperature = new ApexCharts(document.querySelector("#chart-temperature"), optionsTemperature);
       chartTemperature.render();
+
+      // Kelistrikan
+      var optionsElectricity = {
+        chart: {
+          type: 'line',
+          height: '300',
+          fontFamily: 'Ubuntu, sans-serif',
+        },
+        series: [{
+          name: 'sales',
+          data: [null,null,null,null,null,null,null,null,125]
+        }],
+        colors: ['#DE0618'],
+        xaxis: {
+          categories: [1991,1992,1993,1994,1995,1996,1997,1998,1999]
+        }
+      }
+
+      var chartElectricity = new ApexCharts(document.querySelector("#chart-electricity"), optionsElectricity);
+      chartElectricity.render();
     }
   </script>
 @endsection
@@ -47,22 +67,6 @@
   <div class="container-fluid" style="margin-top: 15px; margin-bottom: 15px;">
       <div class="d-sm-flex align-items-center justify-content-between mb-4">
           <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-      </div>
-
-      <div class="col-12 mb-4">
-          <div class="card shadow h-100 py-2">
-              <div class="card-body">
-                  <div class="row no-gutters align-items-top">
-                      <div class="col mr-2">
-                          <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Waktu Beroperasi</div>
-                          <div class="h5 mb-0 font-weight-bold text-gray-800">27.5</div>
-                      </div>
-                      <div class="col-auto">
-                          <i class="fas fa-stopwatch fa-2x text-gray-300" style="color: var(--red);"></i>
-                      </div>
-                  </div>
-              </div>
-          </div>
       </div>
 
       <div class="col-12 mb-4">
@@ -94,7 +98,24 @@
                       </div>
                   </div>
 
-                  <div id="chart-temperature" style="width: 100%;">
+                  <div id="chart-temperature" style="width: 100%;"></div>
+              </div>
+          </div>
+      </div>
+
+      <div class="col-12 mb-4">
+          <div class="card shadow h-100 py-2">
+              <div class="card-body">
+                  <div class="row align-items-top mb-3">
+                      <div class="col mr-5">
+                          <div class="text-xs font-weight-bold text-uppercase mb-1">Kelistrikan</div>
+                      </div>
+                      <div class="col-auto">
+                          <i class="fas fa-bolt fa-2x text-gray-300" style="color: var(--red);"></i>
+                      </div>
+                  </div>
+
+                  <div id="chart-electricity" style="width: 100%;"></div>
               </div>
           </div>
       </div>
