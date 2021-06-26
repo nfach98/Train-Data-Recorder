ef11bad9b9851326c2441d40d463c7c7c71dfc44
Nino Fachrurozy <nfach98@gmail.com>
2021-06-26 17:55:52 +0700

    fix sidebar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,32 +1,62 @@
 @extends('layouts.main')
 
 @section('page')
-  <div class="container-fluid" style="margin-top: 15px; margin-bottom: 15px;">
+  <div class="container-fluid">
       <div class="d-sm-flex align-items-center justify-content-between mb-4">
           <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
           <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
       </div>
 
       <div class="row">
-          <div class="col-xl-4 col-md-12 mb-4">
+          <div class="col-12 mb-4">
               <div class="card shadow h-100 py-2">
                   <div class="card-body">
                       <div class="row no-gutters align-items-top">
                           <div class="col mr-5">
-                              <div class="text-xs font-weight-bold text-uppercase mb-1">Listrik</div>
+                              <div class="text-xs font-weight-bold text-uppercase mb-1">Info Kereta</div>
                               <div class="row">
-                                  <div class="col text-xs">Arus</div>
+                                  <div class="col text-xs">Nama</div>
                                   <div class="col-4 text-right">2</div>
                               </div>
                               <div class="row">
+                                  <div class="col text-xs">ID</div>
+                                  <div class="col-4 text-right">2</div>
+                              </div>
+                          </div>
+                          <div class="col-auto">
+                              <i class="fas fa-subway fa-2x text-gray-300" style="color: var(--red);"></i>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+
+          <div class="col-12 mb-4">
+              <div class="card shadow h-100 py-2">
+                  <div class="card-body">
+                      <div class="row no-gutters align-items-top">
+                          <div class="col mr-5">
+                              <div class="text-xs font-weight-bold text-uppercase mb-1">Kelistrikan</div>
+                              <div class="row">
                                   <div class="col text-xs">Tegangan</div>
+                                  <div class="col-4 text-right">2</div>
+                              </div>
+                              <div class="row">
+                                  <div class="col text-xs">Arus</div>
                                   <div class="col-4 text-right">2</div>
                               </div>
                               <div class="row">
                                   <div class="col text-xs">Frekuensi</div>
                                   <div class="col-4 text-right">2</div>
                               </div>
-                              <!-- <div class="h5 mb-0 font-weight-bold text-gray-800">27.5</div> -->
+                               <div class="row">
+                                  <div class="col text-xs">Daya</div>
+                                  <div class="col-4 text-right">2</div>
+                              </div>
+                               <div class="row">
+                                  <div class="col text-xs">Energi</div>
+                                  <div class="col-4 text-right">2</div>
+                              </div>
                           </div>
                           <div class="col-auto">
                               <i class="fas fa-bolt fa-2x text-gray-300" style="color: var(--red);"></i>
@@ -36,7 +66,7 @@
               </div>
           </div>
 
-          <div class="col-xl-4 col-md-12 mb-4">
+          <div class="col-12 mb-4">
               <div class="card shadow h-100 py-2">
                   <div class="card-body">
                       <div class="row no-gutters align-items-top">
@@ -57,22 +87,6 @@
                           </div>
                           <div class="col-auto">
                               <i class="fas fa-map-marker-alt fa-2x text-gray-300" style="color: var(--red);"></i>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-
-          <div class="col-xl-4 col-md-12 mb-4">
-              <div class="card shadow h-100 py-2">
-                  <div class="card-body">
-                      <div class="row no-gutters align-items-top">
-                          <div class="col mr-2">
-                              <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Waktu Beroperasi</div>
-                              <div class="h5 mb-0 font-weight-bold text-gray-800">27.5</div>
-                          </div>
-                          <div class="col-auto">
-                              <i class="fas fa-stopwatch fa-2x text-gray-300" style="color: var(--red);"></i>
                           </div>
                       </div>
                   </div>
