33c57a0992fdfdb9aa54e06e8e1142c2acde286c
Nino Fachrurozy <nfach98@gmail.com>
2021-07-22 15:48:04 +0700

    route line on progress

--- resources/views/location.blade.php
+++ resources/views/location.blade.php
@@ -22,6 +22,61 @@
               zoom: 15
             });
 
+            function getRoute(start, end) {
+              var url = 'https://api.mapbox.com/directions/v5/mapbox/cycling/' + start[0] + ',' + start[1] + ';' + end[0] + ',' + end[1] + '?steps=true&geometries=geojson&access_token=' + mapboxgl.accessToken;
+
+              $.ajax({
+                url: url,
+                type: "GET",
+                cache: true,
+                dataType: 'json',
+                success: function(data) {
+                  var route = data.routes[0].geometry.coordinates;
+                  console.log(route);
+                  var geojson = {
+                    type: 'Feature',
+                    properties: {},
+                    geometry: {
+                      type: 'LineString',
+                      coordinates: route
+                    }
+                  };
+
+                  if (map.getSource('route')) {
+                    map.getSource('route').setData(geojson);
+                  } else {
+                    map.addLayer({
+                      id: 'route',
+                      type: 'line',
+                      source: {
+                        type: 'geojson',
+                        data: {
+                          type: 'Feature',
+                          properties: {},
+                          geometry: {
+                            type: 'LineString',
+                            coordinates: geojson
+                          }
+                        }
+                      },
+                      layout: {
+                        'line-join': 'round',
+                        'line-cap': 'round'
+                      },
+                      paint: {
+                        'line-color': '#3887be',
+                        'line-width': 5,
+                        'line-opacity': 0.75
+                      }
+                    });
+                  }
+                },
+                error: function(XMLHttpRequest, status, error) { 
+                  console.log(error);
+                }
+              });
+            }
+
             map.addControl(new mapboxgl.GeolocateControl({
                 positionOptions: {
                   enableHighAccuracy: true
@@ -32,7 +87,11 @@
 
             map.addControl(new mapboxgl.NavigationControl());
 
+            map.addControl(new mapboxgl.FullscreenControl());
+
             map.on('load', function () {
+              // getRoute([data.start_longitude, data.start_latitude], [data.end_longitude, data.end_latitude]);
+
               setInterval( function() {
                 $.ajax({
                   url: "/motorcar",
@@ -44,23 +103,37 @@
                   cache: false,
                   dataType: 'json',
                   success: function(data){
-                      map.getSource('train').setData({
-                        'type': 'FeatureCollection',
-                        'features': [
-                          {
-                          'type': 'Feature',
-                          'geometry': {
-                            'type': 'Point',
-                            'coordinates': [data.longitude, data.latitude]
-                            }
-                          }
-                        ]
-                      });
-       
-                      map.flyTo({
-                        center: [data.longitude, data.latitude],
-                        speed: 1
-                      });
+                    var end = {
+                      type: 'FeatureCollection',
+                      features: [{
+                        type: 'Feature',
+                        properties: {},
+                        geometry: {
+                          type: 'Point',
+                          coordinates: [-7.548892, 111.668518]
+                        }
+                      }]
+                    };
+
+                    map.getSource('end').setData(end);
+
+                    // map.getSource('train').setData({
+                    //   'type': 'FeatureCollection',
+                    //   'features': [
+                    //     {
+                    //     'type': 'Feature',
+                    //     'geometry': {
+                    //       'type': 'Point',
+                    //       'coordinates': [data.longitude, data.latitude]
+                    //       }
+                    //     }
+                    //   ]
+                    // });
+     
+                    // map.flyTo({
+                    //   center: [data.longitude, data.latitude],
+                    //   speed: 1
+                    // });
                   },
                   error: function(XMLHttpRequest, status, error) { 
                     console.log(error);
@@ -68,6 +141,29 @@
                 });
               }, 1000);
 
+              map.addLayer({
+                id: 'end',
+                type: 'circle',
+                source: {
+                  type: 'geojson',
+                  data: {
+                    type: 'FeatureCollection',
+                    features: [{
+                      type: 'Feature',
+                      properties: {},
+                      geometry: {
+                        type: 'Point',
+                        coordinates: [-7.548892, 111.668518]
+                      }
+                    }]
+                  }
+                },
+                paint: {
+                  'circle-radius': 10,
+                  'circle-color': '#f30'
+                }
+              });
+              
               map.addSource('train', { 
                 type: 'geojson', 
                 data: {
